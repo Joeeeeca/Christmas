0d9f283b4b03be70d19ee16c9d0a85cb70b9425c
Joeeeeca <joechapon101@gmail.com>
2025-11-29 20:46:29 +0000

    Fixed auth code

--- api/auth.js
+++ api/auth.js
@@ -1,13 +1,11 @@
 export default async function handler(req, res) {
   const clientId = process.env.GITHUB_CLIENT_ID;
 
-  // 🔒 Hard-code your real site URL for now
-  const siteUrl = "https://highfieldroadchristmaslights.com";
-
-  const redirectUri = `${siteUrl}/api/callback`;
+  const redirectUri = "https://highfieldroadchristmaslights.com/api/callback";
 
   const githubUrl =
-    `https://github.com/login/oauth/authorize?client_id=${clientId}` +
+    "https://github.com/login/oauth/authorize" +
+    `?client_id=${clientId}` +
     `&redirect_uri=${encodeURIComponent(redirectUri)}` +
     `&scope=repo`;
 
