282dddeb2cb8202238d964b80c5f0f1a349792c9
Joeeeeca <joechapon101@gmail.com>
2025-11-29 19:54:55 +0000

    Moved admin folder out of public

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  publicDir: "static",
 
   resolve: {
     alias: {
@@ -14,6 +15,7 @@ export default defineConfig({
 
   build: {
     copyPublicDir: true,
+    emptyOutDir: false
   },
 
   server: {
