300c4d720a108ccc3fd44598068c95df27f3057a
Joeeeeca <joechapon101@gmail.com>
2025-12-01 10:58:04 +0000

    Editing post message in callback.js

--- api/callback.js
+++ api/callback.js
@@ -16,9 +16,11 @@ export default function handler(req, res) {
       <body>
         <script>
           if (window.opener) {
-            window.opener.postMessage(
-              'authorization:github:success:${JSON.stringify({ code })}',
-              window.location.origin
+window.opener.postMessage(
+  'authorization:github:success:${JSON.stringify({ code })}',
+  '*'
+);
+
             );
             window.close();
           }
